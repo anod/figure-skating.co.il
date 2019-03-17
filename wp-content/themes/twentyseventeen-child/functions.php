d2ded0c30aef1123e22a4e6c92be9d2c52b4eeb8
Alex Gavrishev <alex.gavrishev@gmail.com>
2019-03-17 20:05:56 +0200

    rtl.css

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -2,10 +2,6 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    if (is_rtl()) {
-        wp_enqueue_style( 'parent-style-rtl', get_template_directory_uri() . '/rtl.css' );
-    }
-
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         ['parent-style']

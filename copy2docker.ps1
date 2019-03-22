5d225ba10b25ae62efe0ba1134b99b1dd5a7c190
Alex Gavrishev <alex.gavrishev@gmail.com>
2019-03-22 11:36:46 +0200

    Use featured image as custom header

---
+++ copy2docker.ps1
@@ -0,0 +1,1 @@
+docker cp .\wp-content\themes\twentyseventeen-child wordpress_wordpress_1:/var/www/html/wp-content/themes/
--- wp-content/themes/twentyseventeen-child/template-parts/header/header-image.php
+++ wp-content/themes/twentyseventeen-child/template-parts/header/header-image.php
@@ -10,9 +10,15 @@
 
 ?>
 <div class="custom-header">
-
+		
 		<div class="custom-header-media">
-			<?php the_custom_header_markup(); ?>
+			<?php if (is_page() && has_post_thumbnail(get_queried_object_id())) : ?>
+				<div id="wp-custom-header" class="wp-custom-header">
+             	<?php echo get_the_post_thumbnail( get_queried_object_id(), 'twentyseventeen-featured-image' ); ?>
+				</div>
+			<?php else: ?>
+				<?php the_custom_header_markup(); ?>
+	        <?php endif; ?>
 		</div>
 
 	<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
